294421b44856161297e02abd84c65b446918c8e9
Jose Garcia <jose.garcia051@gmail.com>
2024-10-02 22:40:55 -0400

    updates to make sure it works as expected

--- recursion/normal/permutations.py
+++ recursion/normal/permutations.py
@@ -1,16 +1,16 @@
-def permutations(array):
-    permutations = []
-    getPermuations(0, array, permutations)
+def permutations(array: list[int]) -> list[int]:
+    permutations: list[int] = []
+    getPermutations(0, array, permutations)
     return permutations
 
-def getPermutations(idx, array, permutation):
+def getPermutations(idx: int, array: list[int], permutations: list[int]):
     if idx == len(array) - 1:
         permutations.append(array[:])
     else:
-        for j in range(idx, len(array));
-            swap(array, idx, j)
+        for j in range(idx, len(array)):
+            swap(idx, j, array)
             getPermutations(idx + 1, array, permutations)
-            swap(array, idx, j)
+            swap(idx, j, array)
 
-def swap(i, j, array):
-    arra[i], array[j] = array[j], array[i]
+def swap(i: int, j: int, array: list[int]):
+    array[i], array[j] = array[j], array[i]
