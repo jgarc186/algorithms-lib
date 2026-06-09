62e9f4dcf4f0a26a1dbf27788849799fff09a9d0
Jose Garcia <jose@specbooks.com>
2026-06-09 13:04:38 -0400

    updated the phone number mnemonic algorithm

--- recursion/normal/phone_number_mnemonic.mjs
+++ recursion/normal/phone_number_mnemonic.mjs
@@ -1,35 +1,35 @@
 function phoneNumberMnemonic(phoneNumber) {
-    let current = [];
+    const current = Array(phoneNumber.length - 1).fill("0")
     const mnemonic = [];
     getMnemonic(0, phoneNumber, current, mnemonic);
-    return mnemonic[mnemonic.length - 1].join('');
+    return mnemonic
 }
 
 function getMnemonic(idx, phone, current, mnemonic) {
     if (idx === phone.length) {
-        mnemonic.push(current);
+        mnemonic.push(current.join(""));
     } else {
         const digit = phone[idx];
         const letters = DIGIT_LETTERS[digit];
-        letters.forEach(letter => {
-            current[idx] = letter;
-            getMnemonic(idx + 1, phone, current, mnemonic);
-        });
+        for (const letter of letters) {
+          current[idx] = letter
+          getMnemonic(idx + 1, phone, current, mnemonic)
+        }
     }
 }
 
 const DIGIT_LETTERS = {
-    '0': ['0'],
-    '1': ['1'],
-    '2': ['A', 'B', 'C'],
-    '3': ['D', 'E', 'F'],
-    '4': ['G', 'H', 'I'],
-    '5': ['J', 'K', 'L'],
-    '6': ['M', 'N', 'O'],
-    '7': ['P', 'Q', 'R', 'S'],
-    '8': ['T', 'U', 'V'],
-    '9': ['W', 'X', 'Y', 'Z']
-}
+    "0": ["0"],
+    "1": ["1"],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
+};
 
 const expect = 'ZLIIL1SFSS';
 const mnemonic = phoneNumberMnemonic('9544517377');
